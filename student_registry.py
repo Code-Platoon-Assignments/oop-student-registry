46736bd070cf65a0837dc9e02f9cf2c624197cc9
Adam Cahan <adam@codeplatoon.org>
2024-10-15 09:27:31 -0500

    Change getter/setter naming convention
    Drop `get_` and `set_` just use the property name.

--- student_registry.py
+++ student_registry.py
@@ -5,33 +5,33 @@ class Student:
         self._grade = grade
 
     @property
-    def get_name(self):
+    def name(self):
         return self._name
 
-    @get_name.setter
-    def set_name(self, new_name):
+    @name.setter
+    def name(self, new_name):
         if isinstance(new_name, str) and len(new_name) >= 3 and new_name.isalpha():
             self._name = new_name.title()
         else:
             print("Invalid name format.")
 
     @property
-    def get_age(self):
+    def age(self):
         return self._age
 
-    @get_age.setter
-    def set_age(self, new_age):
+    @age.setter
+    def age(self, new_age):
         if isinstance(new_age, int) and 11 < new_age < 18:
             self._age = new_age
         else:
             print("Invalid age value.")
 
     @property
-    def get_grade(self):
+    def grade(self):
         return self._grade
 
-    @get_grade.setter
-    def set_grade(self, new_grade):
+    @grade.setter
+    def grade(self, new_grade):
         valid_grades = ["9th", "10th", "11th", "12th"]
         if isinstance(new_grade, str) and new_grade in valid_grades:
             self._grade = new_grade
@@ -39,13 +39,13 @@ class Student:
             print("Invalid grade value.")
 
     def __str__(self):
-        return f"Student 1: Name: {self.get_name}, Age: {self.get_age}, Grade: {self.get_grade}"
+        return f"Student 1: Name: {self.name}, Age: {self.age}, Grade: {self.grade}"
 
     def advance(self, years_advanced=1):
-        self.set_grade = f"{int(self.get_grade[0]) + years_advanced}th"
-        print(f"{self.get_name} has advanced to the {self.get_grade} grade.")
+        self.grade = f"{int(self.grade[0]) + years_advanced}th"
+        print(f"{self.name} has advanced to the {self.grade} grade.")
 
     def study(self, subject):
-        print(f"{self.get_name} is studying {subject}")
+        print(f"{self.name} is studying {subject}")
 
 
